51223058440dd83342b58bc0636c43b4e4277f70
jiangzaixing <jiangzaixing@staff.hexun.com>
2017-07-10 17:01:28 +0800

    666

--- src/main/java/com/company/project/Test.java
+++ src/main/java/com/company/project/Test.java
@@ -14,7 +14,7 @@ public class Test {
         String key = "book";  
         boolean contains = map.containsKey(key);  
         if (contains) {  
-            System.out.println("在Map集合中包含键名11112222333344445555" + key);  
+            System.out.println("在Map集合中包含键名111122223333444455556666" + key);  
         } else {  
             System.out.println("在Map集合中不包含键名" + key);  
         }  
